bee867217e19421d914434aa40e9f981a168b984
Giedrius Jocius <giedrius393@gmail.com>
2023-08-30 08:17:15 +0300

    initial commit

---
+++ backend/src/main/java/com/example/chatapp/Greeting.java
@@ -0,0 +1,12 @@
+package com.example.chatapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+  @Getter
+  private String content;
+}
---
+++ backend/src/main/java/com/example/chatapp/HelloMessage.java
@@ -0,0 +1,14 @@
+package com.example.chatapp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloMessage {
+  @Getter
+  @Setter
+  private String name;
+}
