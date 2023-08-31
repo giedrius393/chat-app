9dcb75ed68c40da98c0a306a738a073951f04985
Giedrius Jocius <giedrius.jocius@devbridge.com>
2023-08-31 16:15:41 +0300

    Add message

--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -1,1 +1,17 @@
+<script>
+  let message = '';
+
+  const onSubmit = () => {
+    console.log(message);
+    message = '';
+  }
+</script>
+
+
 <h1>This is a chat page</h1>
+
+<form on:submit|preventDefault={onSubmit}>
+  <label>
+    Enter message: <input type="text" id="message" bind:value={message}/>
+  </label>
+</form>
